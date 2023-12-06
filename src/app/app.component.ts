dede332a2d77dc94b2a2ea4a48261ca9674c0bf4
TheDarklightsun <bublik.kirill2012@gmail.com>
2023-12-07 01:28:57 +0200

    add-advanced-transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {state, style, trigger} from "@angular/animations";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
@@ -8,27 +8,47 @@ import {state, style, trigger} from "@angular/animations";
   animations: [
     trigger('divState', [
       state('normal', style({
-        'background-color': 'red',
+        'background-color': 'orange',
         transform: 'translateX(0)'
       })),
       state('highlighted', style({
         backgroundColor: 'blue',
-        transform: 'translateX(100px)'
-      }))
+        transform: 'translateX(600px)'
+      })),
+      transition('normal <=> highlighted', animate(500)),
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'orange',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(600px) scale(1)'
+      })),
+      state('shrunken', style({
+        backgroundColor: 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(500)),
+      transition('highlighted => normal', animate(500)),
+      transition('shrunken <=> *', animate(500))
     ])
   ]
 })
 export class AppComponent {
   title = 'app-angular-animations';
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
   }
 
   onShrink() {
-
+    this.wildState = 'shrunken';
   }
 
   onAdd(item) {
