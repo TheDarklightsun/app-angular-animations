88621308a9ebe234ea56d21823c4cee232989a1b
TheDarklightsun <bublik.kirill2012@gmail.com>
2023-12-07 01:50:10 +0200

    add-void-state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,32 +8,60 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
   animations: [
     trigger('divState', [
       state('normal', style({
-        'background-color': 'orange',
+        'background-color': 'red',
         transform: 'translateX(0)'
       })),
       state('highlighted', style({
-        backgroundColor: 'blue',
-        transform: 'translateX(600px)'
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
       })),
-      transition('normal <=> highlighted', animate(500)),
+      transition('normal <=> highlighted', animate(300)),
+      // transition('highlighted => normal', animate(800))
     ]),
     trigger('wildState', [
       state('normal', style({
-        'background-color': 'orange',
+        'background-color': 'red',
         transform: 'translateX(0) scale(1)'
       })),
       state('highlighted', style({
-        backgroundColor: 'blue',
-        transform: 'translateX(600px) scale(1)'
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
       })),
       state('shrunken', style({
-        backgroundColor: 'green',
+        'background-color': 'green',
         transform: 'translateX(0) scale(0.5)'
       })),
-      transition('normal => highlighted', animate(500)),
-      transition('highlighted => normal', animate(500)),
-      transition('shrunken <=> *', animate(500))
-    ])
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          'background-color': 'orange'
+        }),
+        animate(0, style({
+          borderRadius: '50px'
+        })),
+        animate(500)
+      ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ]),
+    ]),
   ]
 })
 export class AppComponent {
